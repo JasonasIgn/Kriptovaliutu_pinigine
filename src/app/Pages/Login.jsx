996adc277e3d9cc7c7a0d9c57187dd37c5441636
JasonasIgn <ignas.jasonas@gmail.com>
2018-11-05 00:52:04 +0200

    Add login form

--- src/app/Pages/Login.jsx
+++ src/app/Pages/Login.jsx
@@ -2,7 +2,26 @@ import React from "react";
 
 const Login = () => (
   <div className="login">
-    <div className="container">asdas</div>
+    <div className="container">
+      <form>
+        <h1 className="title"> Prisijungti</h1>
+        <div class="form-group">
+          <label class="col-form-label" for="inputDefault">
+            El. paštas
+          </label>
+          <input type="text" class="form-control" id="inputDefault" />
+        </div>
+        <div class="form-group">
+          <label class="col-form-label" for="inputDefault">
+            Slaptažodis
+          </label>
+          <input type="text" class="form-control" id="inputDefault" />
+        </div>
+        <button type="button" class="btn btn-primary">
+          Prisijungti
+        </button>
+      </form>
+    </div>
   </div>
 );
 export default Login;

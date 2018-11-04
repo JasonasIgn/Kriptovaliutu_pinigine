40a2904de8f9fcd2258466488b4034ddf9ec4064
JasonasIgn <ignas.jasonas@gmail.com>
2018-11-04 21:00:45 +0200

    Add bootstrap

--- src/app/Components/Pages/Index.jsx
+++ src/app/Components/Pages/Index.jsx
@@ -1,4 +1,11 @@
 import React from "react";
 
-const Index = () => <div> Pagrindas </div>;
+const Index = () => (
+  <div className="container">
+    <h1>Pagrindas</h1>
+    <button type="button" class="btn btn-primary">
+      Primary
+    </button>
+  </div>
+);
 export default Index;
--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./app";
+import App from "./App";
+import "../resources/styles/bootstrap.css";
 
 ReactDOM.render(<App />, document.getElementById("app"));

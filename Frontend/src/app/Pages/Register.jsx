5c07382e6493824fd338d0057beffe008797962e
JasonasIgn <ignas.jasonas@gmail.com>
2018-12-09 21:53:04 +0200

    Add backend folder

--- Frontend/src/app/Pages/Register.jsx
+++ Frontend/src/app/Pages/Register.jsx
@@ -1,45 +1,57 @@
 import React from "react";
 
-const Register = () => (
-  <div className="register">
-    <div className="container">
-      <form>
-        <h1 className="title"> Registracija</h1>
-        <div className="form-group">
-          <label className="col-form-label" htmlFor="firstName">
-            Vardas
-          </label>
-          <input type="text" className="form-control" id="firstName" />
+class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="register">
+        <div className="container">
+          <form>
+            <h1 className="title"> Registracija</h1>
+            <div className="form-group">
+              <label className="col-form-label" htmlFor="firstName">
+                Vardas
+              </label>
+              <input type="text" className="form-control" id="firstName" />
+            </div>
+            <div className="form-group">
+              <label className="col-form-label" htmlFor="lastName">
+                Pavardė
+              </label>
+              <input type="text" className="form-control" id="lastName" />
+            </div>
+            <div className="form-group">
+              <label className="col-form-label" htmlFor="email">
+                El. paštas
+              </label>
+              <input type="email" className="form-control" id="email" />
+            </div>
+            <div className="form-group">
+              <label className="col-form-label" htmlFor="password">
+                Slaptažodis
+              </label>
+              <input type="password" className="form-control" id="password" />
+            </div>
+            <div className="form-group">
+              <label className="col-form-label" htmlFor="repeatPassword">
+                Pakartokite slaptažodį
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="repeatPassword"
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Registruotis
+            </button>
+          </form>
         </div>
-        <div className="form-group">
-          <label className="col-form-label" htmlFor="lastName">
-            Pavardė
-          </label>
-          <input type="text" className="form-control" id="lastName" />
-        </div>
-        <div className="form-group">
-          <label className="col-form-label" htmlFor="email">
-            El. paštas
-          </label>
-          <input type="email" className="form-control" id="email" />
-        </div>
-        <div className="form-group">
-          <label className="col-form-label" htmlFor="password">
-            Slaptažodis
-          </label>
-          <input type="password" className="form-control" id="password" />
-        </div>
-        <div className="form-group">
-          <label className="col-form-label" htmlFor="repeatPassword">
-            Pakartokite slaptažodį
-          </label>
-          <input type="password" className="form-control" id="repeatPassword" />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Registruotis
-        </button>
-      </form>
-    </div>
-  </div>
-);
+      </div>
+    );
+  }
+}
 export default Register;

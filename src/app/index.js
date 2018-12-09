aad2b1b57b08c98d4f74a398bb080d3eca9b4596
JasonasIgn <ignas.jasonas@gmail.com>
2018-12-09 15:11:29 +0200

    Delete files

--- src/app/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "../resources/styles/bootstrap.css";
-
-ReactDOM.render(<App />, document.getElementById("app"));

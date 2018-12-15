370917f8920a7fae16c3566e11c7445a7a509bb1
JasonasIgn <ignas.jasonas@gmail.com>
2018-12-15 16:44:21 +0200

    Add registration

--- Backend/api/paskyra/create.php
+++ Backend/api/paskyra/create.php
@@ -2,7 +2,7 @@
 // required headers
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: POST,OPTIONS");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
  
@@ -23,48 +23,56 @@ $pinigine = new Pinigine($db);
 $data = json_decode(file_get_contents("php://input"));
  
 // make sure data is not empty
-if(
-    !empty($data->Vardas) &&
-    !empty($data->Pavarde) &&
-    !empty($data->El_pastas) &&
-    !empty($data->Slaptazodis)
-){
- 
-	
-    // set product property values
-    $paskyra->Vardas = $data->Vardas;
-    $paskyra->Pavarde = $data->Pavarde;
-    $paskyra->El_pastas = $data->El_pastas;
-    $paskyra->Slaptazodis = $data->Slaptazodis;
-	$paskyra->Teises = 1;
-	$paskyra->Blokuota = 0;
- 
-	$code = $paskyra->create();
-    // create the product
-    if($code == 0){
-		$code = $pinigine->create($paskyra->getId($paskyra->El_pastas));
-        // set response code - 201 created
-        http_response_code(201);
- 
-        // tell the user
-        echo json_encode(array("message" => "Paskyra sukurta sekmingai"));
-    }
-	else {
- 
-        // set response code - 503 service unavailable
-        http_response_code(400);
-		if ($code == 1062) echo json_encode(array("message" => "Toks el. pastas jau uzregistruotas"));
-		else echo json_encode(array("message" => $code));
-    }
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+     // The request is using the POST method
+	 http_response_code(200);
+	 
+	 echo json_encode(array("msesage" => "Viskas ok"));
 }
- 
-// tell the user data is incomplete
 else {
- 
-    // set response code - 400 bad request
-    http_response_code(400);
- 
-    // tell the user
-    echo json_encode(array("message" => "Ne visi duomenys uzpildyti"));
+	if(
+		!empty($data->Vardas) &&
+		!empty($data->Pavarde) &&
+		!empty($data->El_pastas) &&
+		!empty($data->Slaptazodis)
+	){
+	 
+		
+		// set product property values
+		$paskyra->Vardas = $data->Vardas;
+		$paskyra->Pavarde = $data->Pavarde;
+		$paskyra->El_pastas = $data->El_pastas;
+		$paskyra->Slaptazodis = $data->Slaptazodis;
+		$paskyra->Teises = 1;
+		$paskyra->Blokuota = 0;
+	 
+		$code = $paskyra->create();
+		// create the product
+		if($code == 0){
+			$code = $pinigine->create($paskyra->getId($paskyra->El_pastas));
+			// set response code - 201 created
+			http_response_code(201);
+	 
+			echo json_encode(array("message" => "Paskyra sukurta"));
+			// tell the user
+		}
+		else {
+	 
+			// set response code - 503 service unavailable
+			http_response_code(200);
+			if ($code == 1062) echo json_encode(array("message" => "Toks el. pastas jau uzregistruotas"));
+			else echo json_encode(array("message" => $code));
+		}
+	}
+	 
+	// tell the user data is incomplete
+	else {
+	 
+		// set response code - 400 bad request
+		http_response_code(200);
+	 
+		// tell the user
+		echo json_encode(array("message" => "Ne visi duomenys uzpildyti"));
+	}
 }
 ?>
--- Frontend/src/app/Pages/Register.jsx
+++ Frontend/src/app/Pages/Register.jsx
@@ -1,10 +1,89 @@
 import React from "react";
+import request from "superagent";
 
 class Register extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+
+  handleRegistration() {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    document.querySelectorAll(".error").forEach(item => {
+      item.innerHTML = "";
+    });
+    let valid = true;
+    const firstNameSelector = "firstName";
+    const lastNameSelector = "lastName";
+    const emailSelector = "email";
+    const passwordSelector = "password";
+    const passwordRepeatSelector = "repeatPassword";
+
+    const responseField = document.querySelector(".response");
+    responseField.innerHTML = "";
+
+    const firstNameField = document.getElementById(firstNameSelector);
+    const lastNameField = document.getElementById(lastNameSelector);
+    const emailField = document.getElementById(emailSelector);
+    const passwordField = document.getElementById(passwordSelector);
+    const repeatPasswordField = document.getElementById(passwordRepeatSelector);
+
+    const firstNameErrorField = document.querySelector(`.${firstNameSelector}`);
+    const lastNameErrorField = document.querySelector(`.${lastNameSelector}`);
+    const emailErrorField = document.querySelector(`.${emailSelector}`);
+    const passwordErrorField = document.querySelector(`.${passwordSelector}`);
+    const repeatPasswordErrorField = document.querySelector(
+      `.${passwordRepeatSelector}`
+    );
+
+    if (!firstNameField.value) {
+      firstNameErrorField.innerHTML = "Laukas privalomas";
+      valid = false;
+    }
+    if (!lastNameField.value) {
+      lastNameErrorField.innerHTML = "Laukas privalomas";
+      valid = false;
+    }
+    if (!emailField.value) {
+      emailErrorField.innerHTML = "Laukas privalomas";
+      valid = false;
+    } else if (!re.test(emailField.value)) {
+      emailErrorField.innerHTML = "Patikrinkite duomenis";
+      valid = false;
+    }
+    if (!passwordField.value) {
+      passwordErrorField.innerHTML = "Laukas privalomas";
+      valid = false;
+    }
+    if (!repeatPasswordField.value) {
+      repeatPasswordErrorField.innerHTML = "Laukas privalomas";
+      valid = false;
+    } else if (passwordField.value !== repeatPasswordField.value) {
+      repeatPasswordErrorField.innerHTML = "Slaptažodžiai nesutampa";
+      valid = false;
+    }
+
+    if (valid) {
+      request
+        .post("http://localhost/api/paskyra/create.php")
+        .send({
+          Vardas: firstNameField.value,
+          Pavarde: lastNameField.value,
+          El_pastas: emailField.value,
+          Slaptazodis: passwordField.value
+        })
+        .set("Content-Type", "application/json")
+        .then(res => {
+          responseField.innerHTML = res.body.message;
+          console.log(res);
+        })
+        .catch(err => {
+          console.dir(err);
+          //responseField.innerHTML = err.body.message;
+        });
+    }
+  }
+
   render() {
     return (
       <div className="register">
@@ -16,24 +95,28 @@ class Register extends React.Component {
                 Vardas
               </label>
               <input type="text" className="form-control" id="firstName" />
+              <span className="error firstName" />
             </div>
             <div className="form-group">
               <label className="col-form-label" htmlFor="lastName">
                 Pavardė
               </label>
               <input type="text" className="form-control" id="lastName" />
+              <span className="error lastName" />
             </div>
             <div className="form-group">
               <label className="col-form-label" htmlFor="email">
                 El. paštas
               </label>
               <input type="email" className="form-control" id="email" />
+              <span className="error email" />
             </div>
             <div className="form-group">
               <label className="col-form-label" htmlFor="password">
                 Slaptažodis
               </label>
               <input type="password" className="form-control" id="password" />
+              <span className="error password" />
             </div>
             <div className="form-group">
               <label className="col-form-label" htmlFor="repeatPassword">
@@ -44,8 +127,14 @@ class Register extends React.Component {
                 className="form-control"
                 id="repeatPassword"
               />
+              <span className="error repeatPassword" />
             </div>
-            <button type="submit" className="btn btn-primary">
+            <div className="response" />
+            <button
+              type="button"
+              onClick={this.handleRegistration}
+              className="btn btn-primary"
+            >
               Registruotis
             </button>
           </form>

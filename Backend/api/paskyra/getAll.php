a65ac3c2ff345c4f58bdc9b0ec8c491876fc8594
Donatas <donceburz@gmail.com>
2018-12-16 16:58:43 +0200

    Adminas

---
+++ Backend/api/paskyra/getAll.php
@@ -0,0 +1,43 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+ 
+// include database and object files
+include_once '../config/database.php';
+include_once '../objects/paskyra.php';
+ 
+// instantiate database and product object
+$database = new Database();
+$db = $database->getConnection();
+ 
+// initialize object
+$paskyra = new Paskyra($db);
+ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+     // The request is using the POST method
+	 http_response_code(200);
+	 
+	 echo json_encode(array("msesage" => "Viskas ok"));
+}
+else {
+	$code = $paskyra->getAll();
+	
+	if($code != null)
+	{
+		while($row = mysqli_fetch_assoc($code)) {
+			http_response_code(200);
+			if ($row != null) echo json_encode(array("paskyra" => $row));
+			else echo json_encode(array("message" => "Ivyko klaida"));
+		}
+	}
+	/*http_response_code(200);
+	if ($code != null) echo json_encode(array("paskyra" => $code));
+	else echo json_encode(array("message" => "Ivyko klaida"));*/
+
+}
+ 
+// no products found will be here
+?>
